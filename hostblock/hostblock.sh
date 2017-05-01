67c4a883995b5976ce666bce3d796055a617e0f0
manueldl <mdomlop@gmail.com>
2017-05-01 22:49:30 +0200

    Updated from makefile

--- hostblock/hostblock.sh
+++ hostblock/hostblock.sh
@@ -1,23 +1,34 @@
 #!/bin/sh
-# systemd-hosts-adblock
+# hostblock.sh
+
 umask 0022
 
+blockdir='/etc/hosts.d'
+adblock='http://winhelp2002.mvps.org/hosts.txt'
+syshost='/etc/hosts'
+
 merge() {
-    echo "Merging /etc/hosts.d/*... "
-    cat /etc/hosts.d/* > /etc/hosts
+    echo "Merging "$blockdir"/*... "
+    cat "$blockdir"/* > "$syshost"
 }
 
 adblock() {
     echo "Blocking adds... "
-    adblock='http://winhelp2002.mvps.org/hosts.txt'
     # Grepping only 0.0.0.0 starting lines for security reasons.
-    curl -s "$adblock" | grep ^'0.0.0.0 ' > /etc/hosts.d/adblock.block
+    curl -s "$adblock" | grep ^'0.0.0.0 ' > "$blockdir"/adblock.block
 }
 
 adunblock() {
     echo "Unblocking adds... "
-    rm /etc/hosts.d/*.block
+    rm "$blockdir"/*.block
 }
+
+if [ ! -d "$blockdir" ]
+then
+    mkdir -p "$blockdir" || exit 1
+    echo "WARNING: Moving $syshost to $dir/hosts.original"
+    cat "$syshost" > "$dir/hosts.original"
+fi
 
 for i in "$@"
 do
